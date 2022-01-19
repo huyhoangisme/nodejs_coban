394a2f562c559b520026fa91822fe6b4904b1cb2
unknown <hoangmanuciansss@gmail.cm>
2022-01-20 02:55:18 +0700

    static file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import configViewEngine from "./configs/viewEngine";
 const app = express();
 const port = 8080; //
+configViewEngine(app);
 app.get('/', (req, res) => {
-    res.send("I'm Huy Hoang")
+    res.render('index')
 })
 app.listen(port, () => {
     console.log(`The server is listening at http://localhost:${port}`);
